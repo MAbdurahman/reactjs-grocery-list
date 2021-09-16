ef719eaf38624f3f7c8745c52954cfab48e7c491
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-16 10:07:52 -0500

    Add alert-caution class to index.css and its colors

--- src/app/App.js
+++ src/app/App.js
@@ -17,27 +17,50 @@ function App() {
 	const [list, setList] = useState([]);
 	const [isEditing, setIsEditing] = useState(false);
 	const [editId, setEditId] = useState(null);
-	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+	const [alert, setAlert] = useState({ show: false, msg: '', type:'' });
 	//**************** functions ****************//
 	const showAlert = (show = false, type = '', msg = '') => {
 		setAlert({ show, type, msg });
 	};
 	const clearList = () => {
-		showAlert(true, 'danger', 'empty list');
+		showAlert(true, 'danger', 'list cleared!');
 		setList([]);
 	};
 	const removeItem = id => {
-		showAlert(true, 'danger', 'item removed');
+		showAlert(true, 'danger', 'item removed!');
 		setList(list.filter(item => item.id !== id));
 	};
 	const editItem = id => {
 		const specificItem = list.find(item => item.id === id);
 		setIsEditing(true);
+    showAlert(true, 'caution', 'editing item!');
 		setEditId(id);
 		setName(specificItem.title);
 	};
 	const handleSubmit = e => {
-		e.preventDefault();
+    e.preventDefault();
+		if (!name) {
+			showAlert(true, 'danger', 'enter an item!');
+		} else if (name && isEditing) {
+			setList(
+				list.map(item => {
+					if (item.id === editId) {
+						return { ...item, title: name };
+					}
+					return item;
+				})
+			);
+			setName('');
+			setEditId(null);
+			setIsEditing(false);
+			showAlert(true, 'success', 'successful edit');
+		} else {
+			// showAlert(true, 'success', 'item added');
+			const newItem = { id: new Date().getTime().toString(), title: name };
+
+			setList([...list, newItem]);
+			setName('');
+		}
 	};
 	return (
 		<section className='section-center'>
@@ -60,8 +83,15 @@ function App() {
 					</button>
 				</div>
 			</form>
-			<h2>List</h2>
-			<button className='clear-btn'>clear items</button>
+
+			{list.length > 0 && (
+				<div className='grocery-container'>
+					<List items={list} removeItem={removeItem} editItem={editItem} />
+					<button className='clear-btn' onClick={clearList}>
+						clear items
+					</button>
+				</div>
+			)}
 		</section>
 	);
 }
