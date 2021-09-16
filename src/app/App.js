3078422906394b01e3a40eb0050941b56ba99e49
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-16 08:49:43 -0500

    Complete basic structure of project

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from './../components/list/List';
 import Alert from './../components/alert/Alert';
 
@@ -13,11 +13,56 @@ const getLocalStorage = () => {
 
 function App() {
 	//**************** variables ****************//
+	const [name, setName] = useState('');
+	const [list, setList] = useState([]);
+	const [isEditing, setIsEditing] = useState(false);
+	const [editId, setEditId] = useState(null);
+	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 	//**************** functions ****************//
+	const showAlert = (show = false, type = '', msg = '') => {
+		setAlert({ show, type, msg });
+	};
+	const clearList = () => {
+		showAlert(true, 'danger', 'empty list');
+		setList([]);
+	};
+	const removeItem = id => {
+		showAlert(true, 'danger', 'item removed');
+		setList(list.filter(item => item.id !== id));
+	};
+	const editItem = id => {
+		const specificItem = list.find(item => item.id === id);
+		setIsEditing(true);
+		setEditId(id);
+		setName(specificItem.title);
+	};
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
 	return (
-		<div>
-			<h2>This is App.js</h2>
-		</div>
+		<section className='section-center'>
+			<form className='grocery-form' onSubmit={handleSubmit}>
+				{alert.show && (
+					<Alert {...alert} removeAlert={showAlert} list={list} />
+				)}
+
+				<h3>grocery list</h3>
+				<div className='form-control'>
+					<input
+						type='text'
+						className='grocery'
+						placeholder='e.g. eggs'
+						value={name}
+						onChange={e => setName(e.target.value)}
+					/>
+					<button type='submit' className='submit-btn'>
+						{isEditing ? 'edit' : 'submit'}
+					</button>
+				</div>
+			</form>
+			<h2>List</h2>
+			<button className='clear-btn'>clear items</button>
+		</section>
 	);
 }
 
